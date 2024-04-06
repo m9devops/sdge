dd6f49c14486f6e0d999b3fb4217dd25d545b555
m9 devops <devops@m9digital.com>
2024-04-07 02:25:30 +0530

    1st push
    uploaded to git

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,4 +1,36 @@
 import { i18n, type Locale } from "../../i18n-config";
+import { getDictionary } from "../../get-dictionary";
+import { Locale } from "../../i18n-config";
+import Counter from "./components/counter";
+import LocaleSwitcher from "./components/locale-switcher";
+import styles from './app.css';
+
+import localFont from 'next/font/local'
+ 
+// Font files can be colocated inside of `pages`
+const interstate = localFont({ 
+	src: [
+		{
+			path:  '../../public/fonts/interstate/interstate-black-comp-58b64117a2ad9.woff',
+			weight: '300',
+		},
+		{
+			path:  '../../public/fonts/interstate/interstate-regular-comp-58b641cc0aca1.woff',
+			weight: '400',
+		},
+		{
+			path:  '../../public/fonts/interstate/interstate-bold-cond-58b64162258ca.woff',
+			weight: '700',
+		},
+		{
+			path:  '../../public/fonts/interstate/interstate-black-cond-58b642507872b.woff',
+			weight: '800',
+		}
+	],
+	subsets: ['latin'],
+	display: 'swap',
+})
+
 
 export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }));
@@ -13,7 +45,15 @@ export default function Root({
 }) {
   return (
     <html lang={params.lang}>
-      <body>{children}</body>
+      <body className={interstate.className}>
+        <header className={styles.site_header}>
+          <LocaleSwitcher />
+          <p>Current locale: {params.lang}</p>
+        </header>
+        <main>
+        {children}
+        </main>
+      </body>
     </html>
   );
 }
--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,7 +1,7 @@
 import { getDictionary } from "../../get-dictionary";
 import { Locale } from "../../i18n-config";
-import Counter from "./components/counter";
-import LocaleSwitcher from "./components/locale-switcher";
+import Image from 'next/image'
+import styles from './app.css';
 
 export default async function IndexPage({
   params: { lang },
@@ -11,14 +11,20 @@ export default async function IndexPage({
   const dictionary = await getDictionary(lang);
 
   return (
-    <div>
-      <LocaleSwitcher />
-      <p>Current locale: {lang}</p>
-      <p>
-        This text is rendered on the server:{" "}
-        {dictionary["server-component"].welcome}
-      </p>
-      <Counter dictionary={dictionary.counter} />
-    </div>
+    <section id="hero" className={styles.interstate}>
+      <div className="container mx-auto">
+        <div className="hero-left">
+          <h1>{dictionary.banner.banner_title}</h1>
+        </div>
+        <div class="hero-right">
+          <Image
+            src="/"
+            width="500"
+            height="500"
+            alt="test"
+            />
+        </div>
+      </div>
+    </section>
   );
 }
